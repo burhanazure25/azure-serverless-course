6d842f964b76459d10ecda08a3a633615e737731
Burhan Ali <burhan@globalfuelsupply.com>
2026-07-11 19:27:48 +0400

    add-service-bus

---
+++ infra/service-bus.tf
@@ -0,0 +1,21 @@
+resource "azurerm_servicebus_namespace" "namespace" {
+  name                = "${var.project_name}-${var.environment}-ns"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  sku                 = "Standard"
+  tags = {
+    "course" = "serverless"
+  }
+}
+
+resource "azurerm_servicebus_queue" "order_queue" {
+    name = "notification_queue"
+    namespace_id = azurerm_servicebus_namespace.namespace.id
+    max_delivery_count = 5
+}
+
+resource "azurerm_servicebus_namespace_authorization_rule" "auth_rule" {
+    name = "send-policy"
+    namespace_id = azurerm_servicebus_namespace.namespace.id    
+    send = true
+}
