d7c6ebe36fde6c3b12d0e009448e70b7fefd4503
Burhan Ali <burhan@globalfuelsupply.com>
2026-07-13 15:35:37 +0400

    adding-az-func

---
+++ infra/function-order-process.tf
@@ -0,0 +1,42 @@
+resource "azurerm_linux_function_app" "order-process" {
+    name                       = "order-process-burh-${var.environment}"
+    resource_group_name        = azurerm_resource_group.main.name
+    location                   = azurerm_resource_group.main.location
+    service_plan_id            = azurerm_service_plan.my_plan.id
+    storage_account_name       = azurerm_storage_account.main.name
+    storage_account_access_key = azurerm_storage_account.main.primary_access_key
+    site_config {
+        application_stack {
+            dotnet_version = "9.0"
+            use_dotnet_isolated_runtime = true
+
+        }
+    }
+    app_settings = { 
+        "FUNCTIONS_WORKER_RUNTIME" = "dotnet-isolated"
+        "AzureWebJobsStorage"      = azurerm_storage_account.main.primary_connection_string
+        "BlobContainerName" = azurerm_storage_container.receipts.name
+        "WEBSITE_RUN_FROM_PACKAGE" = "1"
+        "APPINSIGHTS_INSTRUMENTATIONKEY" = azurerm_application_insights.order_insights.instrumentation_key
+        "APPLICATIONINSIGHTS_CONNECTION_STRING" = azurerm_application_insights.order_insights.connection_string
+        "ApplicationInsightsAgent_EXTENSION_VERSION"  = "~3"
+        "ServiceBusConnection" = azurerm_servicebus_namespace_authorization_rule.listen_rule.primary_connection_string
+    }
+    tags = { 
+        "course" = "serverless"
+    }
+}
+
+resource "azurerm_application_insights" "order_insights" {
+    name               = "order-insights-${var.environment}"
+    resource_group_name = azurerm_resource_group.main.name
+    location            = azurerm_resource_group.main.location
+    application_type    = "other"
+    workspace_id        = azurerm_log_analytics_workspace.main.id
+}
+
+resource "azurerm_servicebus_namespace_authorization_rule" "listen_rule" {
+    name                = "listen-rule"
+    namespace_id = azurerm_servicebus_namespace.namespace.id
+    listen = true
+}
